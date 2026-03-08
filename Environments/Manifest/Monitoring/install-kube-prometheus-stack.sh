67bc1f778c5dc6f38935a4032d5ca03a2e434616
0xMordecai <sir.lahrach.mohamed@gmail.com>
2026-03-08 00:13:03 +0000

    Manifest/Monitoring[Add a script to install kube prometheus stack using helm]

---
+++ Environments/Manifest/Monitoring/install-kube-prometheus-stack.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Add the Prometheus Community Helm repository
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
+# Install the kube-prometheus-stack Helm chart
+helm install my-kube-prometheus-stack prometheus-community/kube-prometheus-stack --version 82.10.1
